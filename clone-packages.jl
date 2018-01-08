453b5693daed91a91342cab403c303d2161ea51e
Tamas K. Papp <tkpapp@gmail.com>
2018-01-08 11:39:30 +0100

    CI setup.

---
+++ clone-packages.jl
@@ -0,0 +1,3 @@
+Pkg.clone("https://github.com/tpapp/ContinuousTransformations.jl.git")
+Pkg.clone("https://github.com/tpapp/DynamicHMC.jl.git")
+Pkg.clone("https://github.com/tpapp/DiffWrappers.jl.git")
