0ded67d95bc5290e028f6b13bbc3301b3f79acbe
Tamas K. Papp <tkpapp@gmail.com>
2018-03-07 08:51:45 +0100

    Moved Bernoulli example here from DynamicHMC.jl.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,5 +3,7 @@ using DynamicHMCExamples: rel_path
 using NBInclude
 
 nbinclude(rel_path("Estimation of a covariance matrix.ipynb"))
-
 @test all(ESS .≥ 500)
+
+nbinclude(rel_path("Independent Bernoulli draws.ipynb"))
+@test all(ess_α .≥ 200)
