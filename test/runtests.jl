6338aa93302721a1219a498f1446a292dd90ebf3
Tamas K. Papp <tkpapp@gmail.com>
2018-08-31 10:56:39 +0200

    Generate docs using Literate.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using Test
 using DynamicHMCExamples: rel_path
 using Random
+using Literate
+using Documenter
 
 Random.seed!(UInt32[0x57a97f0d, 0x1a38664c, 0x0dddb228, 0x7dbba96f])
 
@@ -11,3 +13,31 @@ Random.seed!(UInt32[0x57a97f0d, 0x1a38664c, 0x0dddb228, 0x7dbba96f])
     include(rel_path("independent_bernoulli.jl"))
     @test ess_α ≥ 200
 end
+
+
+# generate using Literate
+
+DOCSOURCE = rel_path("../docs/src")
+EXAMPLES = ["independent_bernoulli"]
+
+for example in EXAMPLES
+    Literate.markdown(rel_path("$(example).jl"), DOCSOURCE)
+end
+
+
+# render & deploy using Documenter
+
+cd(joinpath(DOCSOURCE, ".."))
+
+makedocs(modules = Module[],
+         format = :html,
+         sitename = "DynamicHMCExamples.jl",
+         pages = vcat(Any["index.md"],
+                      Any["$(example).md" for example in EXAMPLES]))
+
+deploydocs(repo = "github.com/tpapp/DynamicHMCExamples.jl.git",
+           target = "build",
+           osname = "linux",
+           julia = "1.0",
+           deps = nothing,
+           make = nothing)
